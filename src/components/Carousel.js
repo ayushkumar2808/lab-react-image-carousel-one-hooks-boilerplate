5e8a6e2f3f73507b64de6654be87bbb21ea3783d
Ayush Kumar <115335623+Ayush-28-08@users.noreply.github.com>
2022-12-28 12:30:36 +0530

    done

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { forwardRef, useState } from "react";
 import "./Carousel.css";
 import { images } from "../data/CarouselData";
 
@@ -6,8 +6,40 @@ import { images } from "../data/CarouselData";
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
+
 // complete the function below:
 function Carousel() {
+    const [number,setNumber]= useState(0)
+    
+    
+    const back =()=>{
+        if(number>0){
+        setNumber(number-1)
+        }
+        else if(number===0){
+            setNumber(2)
+        }
+    }
+const forward = ()=>{
+    if(number<2){
+    setNumber(number+1)
+    }
+    else if(number===2){
+        setNumber(0)
+    }
+}
+
+    return(
+        <>
+        <div id="display-flex">
+        <ArrowBackIosIcon onClick={back}/>
+        <div>
+            <img src={images[number].img} alt="" />
+        </div>
+        <ArrowForwardIosIcon onClick={forward}/>
+        </div>
+        </>
+    )
 }
 
 export default Carousel;
